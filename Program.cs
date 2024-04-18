749976de26832de26359948605bc3070fd64ef46
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2024-04-18 22:24:06 +0200

    Creating database context

--- Program.cs
+++ Program.cs
@@ -1,15 +1,18 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using MyBoardsApp.DatabaseContext;
 
-// Add services to the container.
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<MyBoardsContext>
+                (options => options.UseSqlServer(
+                                builder.Configuration.GetConnectionString("MyBoardsConnectionString"))
+                );
 
 WebApplication app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
