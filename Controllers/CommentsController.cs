54abc0ef578d7f00ed26f42a1df8715e60cfe185
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2024-05-06 14:53:29 +0200

    Queries - Where, OrderBy, GroupBy, Select, async

---
+++ Controllers/CommentsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyBoardsApp.DatabaseContext;
+using MyBoardsApp.Entities;
+
+namespace MyBoardsApp.Controllers;
+
+
+[ApiController]
+[Route("api/comments")]
+public class CommentsController
+{
+	private readonly MyBoardsContext _context;
+
+	public CommentsController(MyBoardsContext context)
+	{
+		_context = context;
+	}
+	
+	[HttpGet]
+	public async Task<IEnumerable<Comment>> GetAll()
+	{
+		var comments = await _context.Comments.ToListAsync();
+		return comments;
+	}
+
+	[HttpGet("{year:int},{month:int},{day:int}")]
+	public async Task<IEnumerable<Comment>> GetCommentsNewerThan(int year, int month, int day)
+	{
+		var comments = await _context.Comments
+			.Where(c => c.CreatedAt > new DateTime(year, month, day))
+			.ToListAsync();
+
+		return comments;
+	}
+
+	[HttpGet("{count:int}")]
+	public async Task<IEnumerable<Comment>> GetTopCommentsByDate(int count = 5)
+	{
+		var comments = await _context.Comments.OrderBy(c => c.CreatedAt).Take(count).ToListAsync();
+		return comments;
+	}
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyBoardsApp.DatabaseContext;
+using MyBoardsApp.Entities;
+
+namespace MyBoardsApp.Controllers;
+
+[ApiController]
+[Route("api/data")]
+public class UserController
+{
+	private readonly MyBoardsContext _context;
+
+	public UserController(MyBoardsContext context)
+	{
+		_context = context;
+	}
+
+	[HttpGet]
+	public async Task<IEnumerable<User>> GetAllUsers()
+	{
+		var users = await _context.Users.ToListAsync();
+		return users;
+	}
+	
+}
